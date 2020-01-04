9ea9b4477b42ca7eb277b5f13e797617d320d1d0
Robot <guoyang.xie@outlook.com>
2020-01-04 15:54:31 +0000

    fix bug for anaconda installation

--- install-conda.sh
+++ install-conda.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir zp
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -0 ~/zp/miniconda.sh
+curl -o https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -0 ~/zp/miniconda.sh
 bash ~/zp/miniconda.sh -b -p ~/zp/miniconda
 rm ~/zp/miniconda.sh
 echo "PATH=$PATH:~/zp/miniconda/bin" >> ~/.bashrc
