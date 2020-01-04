7bbb4f86573d03a52bce440f6f18fd6303c52213
Robot <guoyang.xie@outlook.com>
2020-01-04 13:40:33 +0000

    first install conda shell script

---
+++ install-conda.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir zp
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -0 ~/zp/miniconda.sh
+bash ~/zp/miniconda.sh -b -p ~/zp/miniconda
+rm ~/zp/miniconda.sh
+echo "PATH=$PATH:~/zp/miniconda/bin" >> ~/.bashrc
