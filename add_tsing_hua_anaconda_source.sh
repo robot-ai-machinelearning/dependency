3ad4da64c0349556f7f0eba1e5a0ddc153d5bb43
Robot <guoyang.xie@outlook.com>
2020-01-05 08:45:57 +0000

    add tsing hua anaconda source

---
+++ add_tsing_hua_anaconda_source.sh
@@ -0,0 +1,4 @@
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
+conda config --set show_channel_urls yes
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/pytorch/
